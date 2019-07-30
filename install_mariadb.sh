119668ef73d4cb32a76a335db3cc347b5ddf7d9e
makeitworks <makeitworks@sina.com>
2019-07-30 05:30:09 -0400

    add install mariadb script

---
+++ install_mariadb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# under ubuntu 18.04
+
+sudo apt-get install -y software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirrors.shu.edu.cn/mariadb/repo/10.4/ubuntu bionic main'
+
+sudo apt update
+sudo apt install -y mariadb-server
+
+#enter mariadb, and create database user
+# 'sudo mysql -uroot -p'    
+# then enter the sudo command password
+
+# 1. create user
+# create user 'django'@'%'  identified by 'django';
+# 2. grant privalige
+# grant all on *.* to 'django';
