85072f555e59b56199595eba2d6404dc13c3d56c
makeitworks <makeitworks@sina.com>
2019-07-30 03:48:19 -0400

    format script

--- link_openssl.sh
+++ link_openssl.sh
@@ -2,6 +2,7 @@
 
 sudo rm -rf /usr/bin/openssl
 sudo ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
+
 sudo rm -rf /usr/lib/x86_64-linux-gnu/libssl.so.1.1
 sudo ln -s /usr/local/ssl/lib/libssl.so.1.1 /usr/lib/x86_64-linux-gnu/libssl.so.1.1
 
