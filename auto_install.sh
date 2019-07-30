1c875fbc7788fa0dd373f292cdc39f58ab7dc268
makeitworks <makeitworks@sina.com>
2019-07-30 15:21:16 +0800

    split scripts

--- auto_install.sh
+++ auto_install.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-# sudo apt update
-
-# sudo apt install -y gcc g++ autoconf make libreadline-dev
 
 
 
 
 
 
-
-
-
-
-
-# sudo ln -s /usr/local/python374/bin/python3 /usr/bin/python
-# sudo ln - s /usr/local/python374/bin/pip3.7 /usr/bin/pip
 
---
+++ install_requirements.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+sudo apt update
+
+sudo apt install -y gcc g++ autoconf make libreadline-dev
+
