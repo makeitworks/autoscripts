abd4dd0ec06c02206d47ca57d982f6be7031ae96
makeitworks <makeitworks@sina.com>
2019-07-30 03:09:31 -0400

    add auto install scripts

---
+++ link_openssl.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo rm -rf /usr/bin/openssl
+sudo ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
+sudo rm -rf /usr/lib/x86_64-linux-gnu/libssl.so.1.1
+sudo ln -s /usr/local/ssl/lib/libssl.so.1.1 /usr/lib/x86_64-linux-gnu/libssl.so.1.1
+
+sudo rm -rf /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1
+sudo ln -s /usr/local/ssl/lib/libcrypto.so.1.1 /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1
