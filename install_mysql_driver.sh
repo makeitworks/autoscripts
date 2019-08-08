e9a6b1f948fb77d22534170b82d387d6feb12754
makeitworks <makeitworks@sina.com>
2019-08-08 10:46:18 +0800

    different os version install libmysqlclient is different

--- install_mysql_driver.sh
+++ install_mysql_driver.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-sudo apt install -y default-libmysqlclient-dev
+
+sudo apt install -y libmysqlclient-dev
+
+os_version=`lsb_release -rs | cut -f1 -d.`
+
+if [ $os_version -eq 16 ] ;
+then
+    sudo apt install -y libmysqlclient-dev
+elif [ $os_version -eq 18 ] ;
+then
+    sudo apt install -y default-libmysqlclient-dev
+fi
+
 
 sudo pip install mysqlclient
