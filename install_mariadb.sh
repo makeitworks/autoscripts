c4457747f214e5b16bb9cddf1beb3ec7e3d3129b
makeitworks <makeitworks@sina.com>
2019-08-14 12:34:52 +0800

    modify

--- install_mariadb.sh
+++ install_mariadb.sh
@@ -11,6 +11,7 @@ if [ $os_version -eq 16 ] ;
 then
     sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://mirror.netcologne.de/mariadb/repo/10.4/ubuntu xenial main'
 elif [ $os_version -eq 18 ] ;
+then
     sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirrors.shu.edu.cn/mariadb/repo/10.4/ubuntu bionic main'
 fi
 
