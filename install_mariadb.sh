264827ac5d5bc6b454859007a8de5736ba72cbf9
makeitworks <makeitworks@sina.com>
2019-08-14 12:33:44 +0800

    modify

--- install_mariadb.sh
+++ install_mariadb.sh
@@ -9,9 +9,7 @@ sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F2
 
 if [ $os_version -eq 16 ] ;
 then
-    
     sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://mirror.netcologne.de/mariadb/repo/10.4/ubuntu xenial main'
-elif
 elif [ $os_version -eq 18 ] ;
     sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirrors.shu.edu.cn/mariadb/repo/10.4/ubuntu bionic main'
 fi
--- install_requirements.sh
+++ install_requirements.sh
@@ -3,5 +3,5 @@
 
 sudo apt update
 
-sudo apt install -y gcc g++ autoconf make libreadline-dev git vim libXpm libXpm-dev
+sudo apt install -y gcc g++ autoconf make libreadline-dev git vim pkg-config
 
