849e785fb9a23b24231f51648ea696fb674b1d89
makeitworks <makeitworks@sina.com>
2019-08-13 22:50:52 -0400

    modify script

--- install_openssl.sh
+++ install_openssl.sh
@@ -18,6 +18,6 @@ cd ${openssl}
 make
 sudo make install
 
-sudo ln -s $openssl_prefix"/lib/pkgconfig/libcrypto.pc"  /usr/lib/pkgconfig/libcrypto.pc
-sudo ln -s $openssl_prefix"/lib/pkgconfig/openssl.pc"  /usr/lib/pkgconfig/openssl.pc
-sudo ln -s $openssl_prefix"/lib/pkgconfig/libssl.pc"  /usr/lib/pkgconfig/libssl.pc
+#sudo ln -s $openssl_prefix"/lib/pkgconfig/libcrypto.pc"  /usr/lib/pkgconfig/libcrypto.pc
+#sudo ln -s $openssl_prefix"/lib/pkgconfig/openssl.pc"  /usr/lib/pkgconfig/openssl.pc
+#sudo ln -s $openssl_prefix"/lib/pkgconfig/libssl.pc"  /usr/lib/pkgconfig/libssl.pc
--- link_openssl.sh
+++ link_openssl.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 sudo rm -rf /usr/bin/openssl
+sudo rm -rf /usr/bin/c_rehash
 sudo ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
+sudo ln -s /usr/local/ssl/bin/c_rehash /usr/bin/c_rehash
 
-sudo rm -rf /usr/lib/x86_64-linux-gnu/libssl.so.1.1
-sudo ln -s /usr/local/ssl/lib/libssl.so.1.1 /usr/lib/x86_64-linux-gnu/libssl.so.1.1
+sudo ln -s /usr/local/ssl/lib/libcrypto.so /usr/lib/libcrypto.so
+sudo ln -s /usr/local/ssl/lib/libcrypto.so.1.1  /usr/lib/libcrypto.so.1.1  
+sudo ln -s /usr/local/ssl/lib/libssl.so /usr/lib/libssl.so
+sudo ln -s /usr/local/ssl/lib/libssl.so.1.1 /usr/lib/libssl.so.1.1
 
-sudo rm -rf /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1
-sudo ln -s /usr/local/ssl/lib/libcrypto.so.1.1 /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1
+
+
+
+sudo ln -s /usr/local/ssl/lib/pkgconfig/libcrypto.pc /usr/lib/pkgconfig/libcrypto.pc
+sudo ln -s /usr/local/ssl/lib/pkgconfig/libssl.pc /usr/lib/pkgconfig/libssl.pc
+sudo ln -s /usr/local/ssl/lib/pkgconfig/openssl.pc /usr/lib/pkgconfig/openssl.pc
+
+#sudo rm -rf /usr/lib/x86_64-linux-gnu/libssl.so.1.1
+#sudo ln -s /usr/local/ssl/lib/libssl.so.1.1 /usr/lib/x86_64-linux-gnu/libssl.so.1.1
+
+#sudo rm -rf /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1
+#sudo ln -s /usr/local/ssl/lib/libcrypto.so.1.1 /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1
