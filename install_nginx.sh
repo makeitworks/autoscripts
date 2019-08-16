e24415f4c2b7afb767c7e23f34f2e0651cf2997f
makeitworks <makeitworks@sina.com>
2019-08-16 11:28:12 +0800

    script

--- install_nginx.sh
+++ install_nginx.sh
@@ -7,22 +7,22 @@ source ./paths.sh
 nginx="nginx-1.16.0"
 nginx_url="http://nginx.org/download/nginx-1.16.0.tar.gz"
 nginx_prefix="/usr/local/nginx"
-# wget --no-check-certificate ${nginx_url} -O ~/Downloads/${nignx}".tar.gz"
+ wget --no-check-certificate ${nginx_url} -O ~/Downloads/${nignx}".tar.gz"
 
 cd ~/Downloads
 
-# tar zxf ${nignx}".tar.gz"
+ tar zxf ${nignx}".tar.gz"
 cd ${nginx}
 
-./configure --prefix=${nginx_prefix} \
-    --with-pcre=${pcre_source_path} \
-    --with-zlib=${zlib_source_path} \
-    --with-openssl=${openssl_source_path} \
+#./configure --prefix=${nginx_prefix} \
+#    --with-pcre=${pcre_source_path} \
+#    --with-zlib=${zlib_source_path} \
+#    --with-openssl=${openssl_source_path} \
 
-make 
-sudo make install
+#make 
+#sudo make install
 
 
-sudo ln -s /usr/local/nginx/sbin/nginx /usr/bin/nginx
+#sudo ln -s /usr/local/nginx/sbin/nginx /usr/bin/nginx
 
 
